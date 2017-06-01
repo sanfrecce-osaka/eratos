2f9fe67ba4fcd0a4cbf1f2e099f762f7554292c2
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-06-01 10:34:14 +0900

    指定した数までの全ての素数を表示する機能を実装

---
+++ eratos.rb
@@ -0,0 +1,21 @@
+class Eratos
+
+  def initialize(max_number)
+    @max_number    = max_number
+    @numbers       = (2..max_number).to_a
+    @prime_numbers = []
+  end
+
+  def extract_prime_numbers
+    return [] if @max_number < 2
+
+    until @numbers.min > Math.sqrt(@max_number)
+      @prime_numbers.push(@numbers.min)
+      @numbers.select { |number| number % @prime_numbers.max == 0 }.each { |number| @numbers.delete(number) }
+    end
+
+    @prime_numbers.push(@numbers).flatten
+  end
+end
+
+puts Eratos.new(ARGV[0].to_i).extract_prime_numbers.join(', ')
